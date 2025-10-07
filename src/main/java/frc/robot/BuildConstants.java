dee0a6b1a0980729d738ed890b1268a1b442095c
atoorian <atoorian@gmail.com>
2025-10-06 19:37:33 -0700

    pathplanner updates for offseason

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "6a31eb8e1fb82df01ab88bd7937a7c5a50413b5c";
   public static final String GIT_DATE = "2025-10-01 13:53:19 PDT";
   public static final String GIT_BRANCH = "postLA";
-  public static final String BUILD_DATE = "2025-10-06 19:00:49 PDT";
-  public static final long BUILD_UNIX_TIME = 1759802449093L;
+  public static final String BUILD_DATE = "2025-10-06 19:05:42 PDT";
+  public static final long BUILD_UNIX_TIME = 1759802742207L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
