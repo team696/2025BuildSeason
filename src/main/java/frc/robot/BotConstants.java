98cc830778f582c109571839624e40edc97e0756
MelonzBigBoss <umadorwut913@gmail.com>
2025-10-09 09:42:13 -0700

    formated file

--- src/main/java/frc/robot/BotConstants.java
+++ src/main/java/frc/robot/BotConstants.java
@@ -15,106 +15,106 @@ import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
  * Constants for robot mechanisms unrelated to the drive train
  */
 public class BotConstants {
-  // The CAN Bus attached directly to the RoboRIO, used for devices controlling
-  // non-drivetrain mechanisms
-  public static CANBus rioBus;
-  // Used for drivetrain devices (motors, encoders, IMU, etc.). Supports CAN FD.
-  public static CANBus canivoreBus;
-  static {
-    rioBus = new CANBus("rio");
-    canivoreBus = new CANBus("cv");
-  }
+	// The CAN Bus attached directly to the RoboRIO, used for devices controlling
+	// non-drivetrain mechanisms
+	public static CANBus rioBus;
+	// Used for drivetrain devices (motors, encoders, IMU, etc.). Supports CAN FD.
+	public static CANBus canivoreBus;
+	static {
+		rioBus = new CANBus("rio");
+		canivoreBus = new CANBus("cv");
+	}
 
-  public static class Elevator {
-    public static int masterID = 11;
-    public static int slaveId = 10;
-    public static TalonFXConfiguration cfg;
-    static {
-      cfg = new TalonFXConfiguration();
-      cfg.Slot0.kP = 16.;//6.;
-      cfg.Slot0.kG = 0.05;
-      // cfg.Slot0.kS = 0.02;
-      cfg.Slot0.GravityType = GravityTypeValue.Elevator_Static;
-      cfg.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign;
+	public static class Elevator {
+		public static int masterID = 11;
+		public static int slaveId = 10;
+		public static TalonFXConfiguration cfg;
+		static {
+			cfg = new TalonFXConfiguration();
+			cfg.Slot0.kP = 16.;// 6.;
+			cfg.Slot0.kG = 0.05;
+			// cfg.Slot0.kS = 0.02;
+			cfg.Slot0.GravityType = GravityTypeValue.Elevator_Static;
+			cfg.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign;
 
-      cfg.MotionMagic.MotionMagicCruiseVelocity = 100.;
-      cfg.MotionMagic.MotionMagicAcceleration = 120.;
-      //cfg.MotionMagic.MotionMagicJerk= 500;
-      cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-      cfg.CurrentLimits.StatorCurrentLimit = 120.;
-      cfg.CurrentLimits.StatorCurrentLimitEnable = true;
+			cfg.MotionMagic.MotionMagicCruiseVelocity = 100.;
+			cfg.MotionMagic.MotionMagicAcceleration = 120.;
+			// cfg.MotionMagic.MotionMagicJerk= 500;
+			cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+			cfg.CurrentLimits.StatorCurrentLimit = 120.;
+			cfg.CurrentLimits.StatorCurrentLimitEnable = true;
 
-    }
+		}
 
-  }
+	}
 
-  public static class Arm {
-    public static int masterID = 13;
-    public static TalonFXConfiguration cfg = new TalonFXConfiguration();
-    static {
-      cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-      cfg.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
-      cfg.Slot0.kP = 8.;
-      cfg.CurrentLimits.StatorCurrentLimitEnable = true;
-      cfg.CurrentLimits.StatorCurrentLimit = 120.;
-      cfg.MotionMagic.MotionMagicCruiseVelocity = 80.;
-      cfg.MotionMagic.MotionMagicAcceleration = 65.;
+	public static class Arm {
+		public static int masterID = 13;
+		public static TalonFXConfiguration cfg = new TalonFXConfiguration();
+		static {
+			cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+			cfg.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+			cfg.Slot0.kP = 8.;
+			cfg.CurrentLimits.StatorCurrentLimitEnable = true;
+			cfg.CurrentLimits.StatorCurrentLimit = 120.;
+			cfg.MotionMagic.MotionMagicCruiseVelocity = 80.;
+			cfg.MotionMagic.MotionMagicAcceleration = 65.;
 
-      cfg.Slot0.kG = 0;
-    }
-  }
+			cfg.Slot0.kG = 0;
+		}
+	}
 
-  public static class Wrist {
-    public static int motorID = 14;
-    public static TalonFXConfiguration cfg = new TalonFXConfiguration();
-    static {
-      cfg.Slot0.kP = 32.;
-      cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-      cfg.MotionMagic.MotionMagicAcceleration = 80.;
-      cfg.MotionMagic.MotionMagicCruiseVelocity = 40.;
-      cfg.CurrentLimits.StatorCurrentLimitEnable = false;
-      cfg.CurrentLimits.SupplyCurrentLimitEnable = false;
-      cfg.CurrentLimits.StatorCurrentLimit = 120.;
-    }
+	public static class Wrist {
+		public static int motorID = 14;
+		public static TalonFXConfiguration cfg = new TalonFXConfiguration();
+		static {
+			cfg.Slot0.kP = 32.;
+			cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+			cfg.MotionMagic.MotionMagicAcceleration = 80.;
+			cfg.MotionMagic.MotionMagicCruiseVelocity = 40.;
+			cfg.CurrentLimits.StatorCurrentLimitEnable = false;
+			cfg.CurrentLimits.SupplyCurrentLimitEnable = false;
+			cfg.CurrentLimits.StatorCurrentLimit = 120.;
+		}
 
-  }
+	}
 
-  public static class EndEffector {
-    public static int motorID = 15;
-    public static TalonFXConfiguration cfg = new TalonFXConfiguration();
-    static {
-      cfg.CurrentLimits.StatorCurrentLimitEnable = true;
-      cfg.CurrentLimits.StatorCurrentLimit = 80.;
-    }
-  }
+	public static class EndEffector {
+		public static int motorID = 15;
+		public static TalonFXConfiguration cfg = new TalonFXConfiguration();
+		static {
+			cfg.CurrentLimits.StatorCurrentLimitEnable = true;
+			cfg.CurrentLimits.StatorCurrentLimit = 80.;
+		}
+	}
 
-  public static class GroundCoral {
-    public static int angleId = 17;
-    public static int rollerId = 18;
-    public static TalonFXConfiguration angleCfg = new TalonFXConfiguration();
-    public static TalonFXConfiguration rollerCfg = new TalonFXConfiguration();
-    static {
-      angleCfg.Slot0.kP = 1.;
-      angleCfg.CurrentLimits.StatorCurrentLimit = 120;
-      angleCfg.CurrentLimits.StatorCurrentLimitEnable = true;
-      angleCfg.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-      angleCfg.MotionMagic.MotionMagicCruiseVelocity = 60;
-      angleCfg.MotionMagic.MotionMagicAcceleration = 30;
+	public static class GroundCoral {
+		public static int angleId = 17;
+		public static int rollerId = 18;
+		public static TalonFXConfiguration angleCfg = new TalonFXConfiguration();
+		public static TalonFXConfiguration rollerCfg = new TalonFXConfiguration();
+		static {
+			angleCfg.Slot0.kP = 1.;
+			angleCfg.CurrentLimits.StatorCurrentLimit = 120;
+			angleCfg.CurrentLimits.StatorCurrentLimitEnable = true;
+			angleCfg.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+			angleCfg.MotionMagic.MotionMagicCruiseVelocity = 60;
+			angleCfg.MotionMagic.MotionMagicAcceleration = 30;
 
-      rollerCfg.CurrentLimits.StatorCurrentLimit = 120;
-      rollerCfg.CurrentLimits.StatorCurrentLimitEnable = true;
-    }
+			rollerCfg.CurrentLimits.StatorCurrentLimit = 120;
+			rollerCfg.CurrentLimits.StatorCurrentLimitEnable = true;
+		}
 
-  }
-  
-  public static class Climber{
-    public static int motorID=19;
-    public static TalonFXConfiguration cfg=new TalonFXConfiguration();
-    static{
-      cfg.Slot0.kP=1;
-      cfg.CurrentLimits.StatorCurrentLimitEnable=false;
-      
-    }
-  }
+	}
+
+	public static class Climber {
+		public static int motorID = 19;
+		public static TalonFXConfiguration cfg = new TalonFXConfiguration();
+		static {
+			cfg.Slot0.kP = 1;
+			cfg.CurrentLimits.StatorCurrentLimitEnable = false;
+
+		}
+	}
 
 }
