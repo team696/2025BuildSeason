d280992c8deb9a9bff78e744b10de4c41feec24c
atoorian <atoorian@gmail.com>
2025-10-09 14:49:22 -0700

    Some more pathplanner tuning.
    Slowed down max velocity and acceleration slightly.
    Changed waypoints slightly.
    
    Everything works ok except:
    
    Need to end at L2, not L4
    Want the elevator to go to L3 when you move it from L4 to home.
    2nd coral dropoff is slightly off.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025BuildSeason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "dee0a6b1a0980729d738ed890b1268a1b442095c";
-  public static final String GIT_DATE = "2025-10-06 19:37:33 PDT";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "4e21ce352e40bcdffee377356d6ae51a23f118bc";
+  public static final String GIT_DATE = "2025-10-07 17:28:33 PDT";
   public static final String GIT_BRANCH = "2025offseason";
-  public static final String BUILD_DATE = "2025-10-07 15:10:03 PDT";
-  public static final long BUILD_UNIX_TIME = 1759875003420L;
+  public static final String BUILD_DATE = "2025-10-09 14:45:59 PDT";
+  public static final long BUILD_UNIX_TIME = 1760046359786L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
