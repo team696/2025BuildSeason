f4239b08d97991a0dfac80b4c8742f6ecaa5cab5
atoorian <atoorian@gmail.com>
2025-10-10 11:56:58 -0700

    Max accel at 3

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025BuildSeason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 76;
-  public static final String GIT_SHA = "8dad9dc2265b1dec707034fb9f25c3163e5c9d1e";
-  public static final String GIT_DATE = "2025-10-09 16:08:07 PDT";
+  public static final int GIT_REVISION = 77;
+  public static final String GIT_SHA = "6b035fc1dc8c2ccc91b4ac0beb71dcf4190c2859";
+  public static final String GIT_DATE = "2025-10-09 17:59:34 PDT";
   public static final String GIT_BRANCH = "2025offseason";
-  public static final String BUILD_DATE = "2025-10-09 17:52:58 PDT";
-  public static final long BUILD_UNIX_TIME = 1760057578435L;
+  public static final String BUILD_DATE = "2025-10-09 18:31:15 PDT";
+  public static final long BUILD_UNIX_TIME = 1760059875113L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
