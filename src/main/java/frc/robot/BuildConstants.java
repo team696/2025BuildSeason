6b035fc1dc8c2ccc91b4ac0beb71dcf4190c2859
atoorian <atoorian@gmail.com>
2025-10-09 17:59:34 -0700

    Thursday Edits, commited by Brandon

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025BuildSeason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 75;
-  public static final String GIT_SHA = "d280992c8deb9a9bff78e744b10de4c41feec24c";
-  public static final String GIT_DATE = "2025-10-09 14:49:22 PDT";
+  public static final int GIT_REVISION = 76;
+  public static final String GIT_SHA = "8dad9dc2265b1dec707034fb9f25c3163e5c9d1e";
+  public static final String GIT_DATE = "2025-10-09 16:08:07 PDT";
   public static final String GIT_BRANCH = "2025offseason";
-  public static final String BUILD_DATE = "2025-10-09 16:07:16 PDT";
-  public static final long BUILD_UNIX_TIME = 1760051236784L;
+  public static final String BUILD_DATE = "2025-10-09 17:52:58 PDT";
+  public static final long BUILD_UNIX_TIME = 1760057578435L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
