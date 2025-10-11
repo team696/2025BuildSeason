69bcc7a032b8821ba571e112f4d035fc1f4783af
unknown <atoorian@gmail.com>
2025-10-10 20:42:45 -0700

    Turned off loggers.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "f4239b08d97991a0dfac80b4c8742f6ecaa5cab5";
   public static final String GIT_DATE = "2025-10-10 11:56:58 PDT";
   public static final String GIT_BRANCH = "2025offseason";
-  public static final String BUILD_DATE = "2025-10-10 20:29:59 PDT";
-  public static final long BUILD_UNIX_TIME = 1760153399764L;
+  public static final String BUILD_DATE = "2025-10-10 20:34:41 PDT";
+  public static final long BUILD_UNIX_TIME = 1760153681939L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
