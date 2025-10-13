ac4eefe6d687d83e3dfd98318c0bf391b5d91312
atoorian <atoorian@gmail.com>
2025-10-13 14:34:00 -0700

    canged max v back to 3 and max a back to 2.4

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025BuildSeason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 72;
-  public static final String GIT_SHA = "35eb49f4c59a322a36bed9578e58c02cd244ab9c";
-  public static final String GIT_DATE = "2025-10-03 22:02:18 PDT";
+  public static final int GIT_REVISION = 73;
+  public static final String GIT_SHA = "23d68b0cc9b2b5f702ed325f4ee68414f572fd28";
+  public static final String GIT_DATE = "2025-10-06 18:41:54 PDT";
   public static final String GIT_BRANCH = "postLA";
-  public static final String BUILD_DATE = "2025-10-06 17:04:04 PDT";
-  public static final long BUILD_UNIX_TIME = 1759795444883L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-10-13 14:32:18 PDT";
+  public static final long BUILD_UNIX_TIME = 1760391138186L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
