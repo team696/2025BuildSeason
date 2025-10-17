b0f12a5701f077d959404e06b1569cd8a8d66ed2
atoorian <atoorian@gmail.com>
2025-10-17 12:12:03 -0700

    Changed all the swerve CAN device IDs, because a lot of them were conflicting.
    Motor IDs: 0,1 / 2,3 / 4,5 / 6,7
    CanCoder IDs: 20, 22, 24, 26
    Pigeon ID: 30

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025BuildSeason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 81;
-  public static final String GIT_SHA = "2feb592136e54e16e0e03bb52bd6072587474e45";
-  public static final String GIT_DATE = "2025-10-14 15:16:52 PDT";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "6917d1d401a42da4424f42903a1cebd4a01b751d";
+  public static final String GIT_DATE = "2025-10-17 11:58:16 PDT";
   public static final String GIT_BRANCH = "2025offseason";
-  public static final String BUILD_DATE = "2025-10-15 14:42:20 PDT";
-  public static final long BUILD_UNIX_TIME = 1760564540482L;
+  public static final String BUILD_DATE = "2025-10-17 12:07:18 PDT";
+  public static final long BUILD_UNIX_TIME = 1760728038200L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/TunerConstants.java
+++ src/main/java/frc/robot/TunerConstants.java
@@ -90,7 +90,7 @@ public class TunerConstants {
   private static final boolean kInvertLeftSide = false;
   private static final boolean kInvertRightSide = true;
 
-  private static final int kPigeonId = 0;
+  private static final int kPigeonId = 30;
 
   // These are only used for simulation
   private static final MomentOfInertia kSteerInertia = KilogramSquareMeters.of(0.01);
@@ -129,7 +129,7 @@ public class TunerConstants {
   // Front Left
   private static final int kFrontLeftDriveMotorId = 1;
   private static final int kFrontLeftSteerMotorId = 0;
-  private static final int kFrontLeftEncoderId = 0;
+  private static final int kFrontLeftEncoderId = 20;
   private static final Angle kFrontLeftEncoderOffset = Rotations.of(0.15771484375);
   private static final boolean kFrontLeftSteerMotorInverted = true;
   private static final boolean kFrontLeftEncoderInverted = false;
@@ -140,7 +140,7 @@ public class TunerConstants {
   // Front Right
   private static final int kFrontRightDriveMotorId = 3;
   private static final int kFrontRightSteerMotorId = 2;
-  private static final int kFrontRightEncoderId = 1;
+  private static final int kFrontRightEncoderId = 22;
   private static final Angle kFrontRightEncoderOffset = Rotations.of(0.415283203125);
   private static final boolean kFrontRightSteerMotorInverted = true;
   private static final boolean kFrontRightEncoderInverted = false;
@@ -151,7 +151,7 @@ public class TunerConstants {
   // Back Left
   private static final int kBackLeftDriveMotorId = 5;
   private static final int kBackLeftSteerMotorId = 4;
-  private static final int kBackLeftEncoderId = 2;
+  private static final int kBackLeftEncoderId = 24;
   private static final Angle kBackLeftEncoderOffset = Rotations.of(0.448974609375);
   private static final boolean kBackLeftSteerMotorInverted = true;
   private static final boolean kBackLeftEncoderInverted = false;
@@ -162,7 +162,7 @@ public class TunerConstants {
   // Back Right
   private static final int kBackRightDriveMotorId = 7;
   private static final int kBackRightSteerMotorId = 6;
-  private static final int kBackRightEncoderId = 3;
+  private static final int kBackRightEncoderId = 26;
   private static final Angle kBackRightEncoderOffset = Rotations.of(-0.455078125);
   private static final boolean kBackRightSteerMotorInverted = true;
   private static final boolean kBackRightEncoderInverted = false;
